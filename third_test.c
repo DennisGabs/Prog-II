e809f482bda30c158ee695c9624dcb71651cde46
DennisGabs <dennisgabriel9876@gmail.com>
2024-03-05 22:10:05 -0300

    change logic, O(n**n)

--- third_test.c
+++ third_test.c
@@ -10,17 +10,17 @@ void main(){
 
     int maior = vect[0];
     int menor = vect[0];
-    for(int i =0; i< 8; i++){
-        for (int j = 0; j < 8; ++j) {
-            if(vect[j] < menor){
-                menor = vect[j];
-            }
 
-            if(vect[j] > maior){
-                maior = vect[j];
-            }
+    for (int j = 0; j < 8; ++j) {
+        if(vect[j] < menor){
+            menor = vect[j];
+        }
+
+        if(vect[j] > maior){
+            maior = vect[j];
         }
     }
+
 
     int soma = maior + menor;
     printf("A soma do maior e menor valor do array eh: %d", soma);
